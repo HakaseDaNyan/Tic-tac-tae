501dea6ef9d38157245f53743d9fc653ba79d376
HakaseDaNyan <ryan830807@gmail.com>
2015-05-04 21:14:14 +0800

    v.02
    123

--- Server.js
+++ Server.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 var io = require('socket.io');
 var server = http.createServer(handler);
 
-var count=1;
-var arr=[0,0,0,0,0,0,0,0,0];
-var fn=0;
+var count=1;	//回合 1是圈 2是叉
+var arr=[0,0,0,0,0,0,0,0,0];	//每個圖片的值皆為0
+var fn=0;	//輸贏的迴傳值
 
 function handler(request,response){		//網路連線
 	fs.readFile('./client.html',function(err,data){
@@ -30,7 +30,7 @@ function worl(arr){                        //判斷輸贏
 		   (arr[2]==arr[5]&&arr[5]==arr[8]&&arr[8]==1)|| //直行
 		   (arr[0]==arr[4]&&arr[4]==arr[8]&&arr[8]==1)||
 		   (arr[2]==arr[4]&&arr[4]==arr[6]&&arr[6]==1)){ //斜
-			fn=1;
+			fn=1; //圈贏
 			}
 		else if((arr[0]==arr[1]&&arr[1]==arr[2]&&arr[2]==2)||
 		   (arr[3]==arr[4]&&arr[4]==arr[5]&&arr[5]==2)||
@@ -40,10 +40,10 @@ function worl(arr){                        //判斷輸贏
 		   (arr[2]==arr[5]&&arr[5]==arr[8]&&arr[8]==2)|| //直行
 		   (arr[0]==arr[4]&&arr[4]==arr[8]&&arr[8]==2)||
 		   (arr[2]==arr[4]&&arr[4]==arr[6]&&arr[6]==2)){	 //斜
-			fn=2;
+			fn=2; //叉贏
 			}
 		else if(arr[0]!=0&&arr[1]!=0&&arr[2]!=0&&arr[3]!=0&&arr[4]!=0&&arr[5]!=0&&arr[6]!=0&&arr[7]!=0&&arr[8]!=0){
-			fn=3;
+			fn=3; //平手
 	}
 	};
 
@@ -53,19 +53,21 @@ server.listen(3000,function(){
 });
 // 開啟 Socket.IO 的 listener
 var io = io.listen(server);
-
+//接收connect
 io.sockets.on('connect', function (socket) {
 	console.log('connetion!');
-	socket.on('restart',function(){
-		arr=[0,0,0,0,0,0,0,0,0];
+	socket.on('restart',function(){	//等待restart鍵的觸發
+		arr=[0,0,0,0,0,0,0,0,0];	//初始化所有值
 		count=1;
 		fn=0;
 	});
+	//client按下圈或叉後 儲存socket的turn值 並回傳data
 	socket.on('turn',function(turns){
 		socket.turn = turns;
 		console.log("turn="+socket.turn);
 		io.sockets.emit('data',count,arr);
 	});
+	//收到client更改過後的data後傳給所有socket並進行輸贏判斷
 	socket.on('databack',function(count,arr){
 		console.log("count="+count+"arr="+arr);
 		io.sockets.emit('data',count,arr);
